04e8266da322a55daef45caec5e76ee99701d2b0
이대산 <idaesan@idaesan-ui-MacBookPro.local>
2020-05-26 22:33:07 +0900

    homes commit 2

---
+++ homes/src/main/java/com/homes/ds/config/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.homes.ds.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import com.homes.ds.constant.Constant;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		// 로그인 disabled 처리 : (에러로 인해 csrf 임시 막기)
+		http.cors().and();
+		http.csrf().disable();
+		
+		// 접근권한 설정
+		http.authorizeRequests()
+			.antMatchers("/login").permitAll()
+			.anyRequest().authenticated();
+		
+		// 로그인 설정
+		http.formLogin()
+			.loginPage("/" + Constant.VIEWS + "/login")
+			.permitAll();
+	}
+}
