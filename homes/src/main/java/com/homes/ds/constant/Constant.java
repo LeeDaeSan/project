7d46b5768886a7d5359e7ad97ceec69381c5ab52
이대산 <idaesan@idaesan-ui-MacBookPro.local>
2020-05-26 21:58:18 +0900

    home commit

---
+++ homes/src/main/java/com/homes/ds/constant/Constant.java
@@ -0,0 +1,8 @@
+package com.homes.ds.constant;
+
+public class Constant {
+
+	//view default path
+	public static final String VIEWS 	= "views";
+	public static final String NOTILES 	= "notiles";
+}
