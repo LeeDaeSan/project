07d189047e7bfd3c53cb683480de6179d12ddc2e
DAESAN.LEE <oidaesanio@gmail.com>
2020-06-01 14:19:42 +0900

    commit security test

--- homes/src/main/java/com/homes/ds/config/WebSecurityConfig.java
+++ homes/src/main/java/com/homes/ds/config/WebSecurityConfig.java
@@ -1,9 +1,11 @@
 package com.homes.ds.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import com.homes.ds.constant.Constant;
@@ -14,9 +16,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private AuthenticationSuccessHandler authenticationSuccessHandler;
 	
+	@Autowired
+	private AuthenticationFailureHandler authenticationFailureHandler;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		super.configure(auth);
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		System.out.println();
 		// 로그인 disabled 처리 : (에러로 인해 csrf 임시 막기)
 		http.cors().and();
 		http.csrf().disable();
@@ -29,6 +38,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		// 로그인 설정
 		http.formLogin()
 			.loginPage("/" + Constant.VIEWS + "/login")
+			.successHandler(authenticationSuccessHandler)
+			.failureHandler(authenticationFailureHandler)
 			.permitAll();
 	}
 }
