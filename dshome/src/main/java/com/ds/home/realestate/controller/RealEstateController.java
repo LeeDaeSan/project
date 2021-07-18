06d1c834003447adf16351adcbaec5800f667c2f
daesan <oidaesanio@gmail.com>
2021-07-18 17:21:59 +0900

    daesan commit

--- dshome/src/main/java/com/ds/home/realestate/controller/RealEstateController.java
+++ dshome/src/main/java/com/ds/home/realestate/controller/RealEstateController.java
@@ -5,16 +5,23 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.ds.home.model.CityLocation;
 import com.ds.home.realestate.service.RealEstateService;
 
-@Controller
-@RequestMapping("/city")
+/**
+ * 부동산 Rest Controller
+ * 
+ * @author daesan
+ *
+ */
+@RestController
+@RequestMapping("/realEstate")
 public class RealEstateController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(RealEstateController.class);
@@ -22,10 +29,32 @@ public class RealEstateController {
 	@Autowired
 	private RealEstateService realEstateService;
 	
+	/**
+	 * 부동산 목록 Controller
+	 * 
+	 * @param cityLocation
+	 * @return
+	 */
 	@GetMapping("/list")
-	@ResponseBody
-	public List<CityLocation> getCityLocationList(CityLocation cityLocation){
+	public List<CityLocation> list(CityLocation cityLocation){
 		return realEstateService.getCityLocationList(cityLocation.getCortarNo(), cityLocation.getDepth());
 	}
-
+	
+	/**
+	 * 부동산 상세 Controller
+	 * 
+	 */
+	@PostMapping("/detail")
+	public void detail () {
+	}
+	
+	/**
+	 * 부동산 등록, 수정, 삭제 Controller
+	 * 
+	 * @param type
+	 */
+	@PostMapping("/merge")
+	public void merge(@RequestParam("type") String type) {
+	}
+	
 }
