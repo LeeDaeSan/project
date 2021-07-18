f589c22e2a351cbbb7221c7ac1618a8fc16f7637
daesan <oidaesanio@gmail.com>
2021-07-18 17:30:01 +0900

    daesan

---
+++ dshome/src/main/java/com/ds/home/model/MemberRealEstate.java
@@ -0,0 +1,45 @@
+package com.ds.home.model;
+import java.util.Date;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.Data;
+
+/**
+ * 사용자 개인 부동산 정보 Table
+ * 
+ * @author daesan
+ *
+ */
+@Data
+@Alias("memberRealEstate")
+public class MemberRealEstate {
+
+	private Integer memberRealEstateIdx;
+	private Integer memberIdx;
+	private String houseName;
+	private String address;
+	private String stationLine;
+	private String stationName;
+	private String stationRange;
+	private Date useApproveDate;
+	private String totalHouseholdCount;
+	private String parkingPossibleCount;
+	private String directionType;
+	private String bay;
+	private String supplySpace;
+	private String exclusiveSpace;
+	private String dealType;
+	private Long dealPrice;
+	private Long brokerFee;
+	private Long acquisitionTax;
+	private Long propertyTotalTax;
+	private String remark;
+	private Date createDate;
+	private Date updateDate;
+	
+	/**
+	 * 사용자 정보
+	 */
+	private Member member;
+}
